7732dc1d37f9856cb4a124b8c587d5976568b9a0
Arush <arushawasthi3@gmail.com>
2025-08-18 09:34:48 +0530

    add apps.js

--- src/apps.js
+++ src/apps.js
@@ -1,5 +1,8 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
-
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}));
 export default app;
