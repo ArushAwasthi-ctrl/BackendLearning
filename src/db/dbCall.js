4888c967d411396f6751af2b131e99e739c38ccc
Arush <arushawasthi3@gmail.com>
2025-08-19 10:07:20 +0530

    commit

--- src/db/dbCall.js
+++ src/db/dbCall.js
@@ -11,7 +11,7 @@ dotenv.config({
        console.log("Database connected succesfully");
        console.log(currentInstance.connection.host); 
     } catch (error) {
-        console.log("Erorr", error);
+        console.log("Error", error);
         process.exit(1);
         
     }
